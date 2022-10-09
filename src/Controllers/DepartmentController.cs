8c52bf581c3ea25748057b995c2eebf305b1a533
ArsalanHaider <arsalanhaider@outlook.com>
2022-10-09 22:30:42 +0500

    first commit

---
+++ src/Controllers/DepartmentController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Get All Students");
+        }
+
+        [HttpGet(template:"{code}")]
+        public async Task<IActionResult> GetA(string code)
+        {
+            return Ok("Get" + code + " department data");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insert()
+        {
+            return Ok("Insert new department");
+        }
+
+        [HttpPut(template: "{code}")]
+        public async Task<IActionResult> Update(string code)
+        {
+            return Ok("Update " + code + " department");
+        }
+
+        [HttpDelete(template: "{code}")]
+        public async Task<IActionResult> Delete(string code)
+        {
+            return Ok("Delete " + code + " department");
+        }
+    }
+}
